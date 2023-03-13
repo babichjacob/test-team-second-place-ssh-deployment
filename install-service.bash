e2a9138dad3cd835949fb9f35206611914de6101
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-03-13 14:48:48 -0400

    🐛 fix: deal with no permission in `/etc/systemd/system`

--- install-service.bash
+++ install-service.bash
@@ -1,3 +1,4 @@
+sudo cp ./tsp-rust-ssh-deployment.service /etc/systemd/system/tsp-rust-ssh-deployment.service
 sudo systemctl daemon-reload
 sudo systemctl restart tsp-rust-ssh-deployment
 sudo systemctl enable tsp-rust-ssh-deployment
