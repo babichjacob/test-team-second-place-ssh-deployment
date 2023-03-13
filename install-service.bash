8993b1b53cc1936c077c01606c93b400bfdfc08a
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-03-13 15:19:42 -0400

    🐛 fix (attempt): allow the service through SELinux

--- install-service.bash
+++ install-service.bash
@@ -1,3 +1,5 @@
+sudo semanage fcontext -a -t bin_t '/home/opc/test-rust-ssh-deployment'
+sudo restorecon -Fv /home/opc/test-rust-ssh-deployment
 sudo cp ./tsp-rust-ssh-deployment.service /etc/systemd/system/tsp-rust-ssh-deployment.service
 sudo systemctl daemon-reload
 sudo systemctl restart tsp-rust-ssh-deployment
